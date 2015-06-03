82a24eac07e3a4c7f41889fe8b3c9c1eebd631b4
Reed Weichler <rweichler@gmail.com>
2015-06-02 20:04:22 -0700

    reading files "works"
    and by works, i mean i used a hack. the cluster is always
    2 more than what it should be for some reason. idk why.

--- src/main.c
+++ src/main.c
@@ -2,19 +2,20 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define LEN_BOOT 446
-#define LEN_PART 16
-#define NUM_PARTS 4
-#define BEGIN_PART_LBA 8
-#define LEN_PART_LBA 4
+#define DEBUG_ON //comment this out to turn off debug mode
 
-#define errorf(fmt, ...) fprintf(stderr, "\033[0;31merror:\033[0m "fmt , ##__VA_ARGS__)
-#define debugf(fmt, ...) printf("\033[0;32mdebug:\033[0m " fmt, ##__VA_ARGS__)
-/* comment to turn on debug
-#undef debugf
-#define debugf(...)
-//*/
+#define errorf(fmt, ...) fprintf(stderr, "\033[0;31m<error>\033[0m "fmt , ##__VA_ARGS__)
+#ifdef DEBUG_ON
+#define debugf(fmt, ...) printf("\033[0;32m<debug>\033[0m " fmt, ##__VA_ARGS__)
+#else
+#define debugf(fmt, ...)
+#endif
+#define colorf(color, fmt, ...) printf("\033[0;" color "m" fmt "\033[0m", ##__VA_ARGS__)
+#define purplef(fmt, ...) colorf("35", fmt, ##__VA_ARGS__)
+#define redf(fmt, ...) colorf("31", fmt, ##__VA_ARGS__)
+#define greenf(fmt, ...) colorf("32", fmt, ##__VA_ARGS__)
 
 bool read_bytes(FILE *f, unsigned int offset, void *buf, unsigned int length)
 {
@@ -23,10 +24,11 @@ bool read_bytes(FILE *f, unsigned int offset, void *buf, unsigned int length)
         return false;
     }
 
-    if(fgets(buf, length, f) == NULL) {
+    if(length != fread(buf, 1, length, f) && ferror(f)) {
         errorf("error reading file\n");
         return false;
     }
+
 
     return true;
 }
@@ -34,7 +36,7 @@ bool read_bytes(FILE *f, unsigned int offset, void *buf, unsigned int length)
 #define READ_BITS_DECL(NUM_BITS) \
 bool read_i##NUM_BITS(FILE *f, unsigned int offset, uint##NUM_BITS##_t *result)\
 {\
-    char buf[NUM_BITS/8 + 1];\
+    char buf[NUM_BITS/8];\
     if(!read_bytes(f, offset, buf, sizeof(buf))) {\
         return false;\
     }\
@@ -51,6 +53,11 @@ READ_BITS_DECL(16);
 READ_BITS_DECL(32);
 READ_BITS_DECL(64);
 
+#define LEN_BOOT 446
+#define LEN_PART 16
+#define NUM_PARTS 4
+#define BEGIN_PART_LBA 8
+#define LEN_PART_LBA 4
 #define SIG 0xAA55
 bool check_sig(FILE *f)
 {
@@ -69,23 +76,90 @@ struct BPB_t {
     char OEMName[8];
     uint16_t BytsPerSec;
     uint8_t SecPerClus;
-    uint16_t RsvdSecCnt; //reserved sector count
+    uint16_t RsvdSecCnt;
 
     uint8_t NumFATs;
     uint16_t RootEntCnt;
     uint16_t TotSec16;
     uint8_t Media;
     uint16_t FATSz16; //sectors per FAT
-    uint16_t SecPerTrk; //sectors per cluster
+    uint16_t SecPerTrk;
     uint16_t NumHeads;
     uint32_t HiddSec;
 
     uint32_t TotSec32; //total size
+};
+uint32_t BPB_FAT_addr(struct BPB_t *bpb)
+{
+    return bpb->RsvdSecCnt * bpb->BytsPerSec;
+}
+uint32_t BPB_Root_addr(struct BPB_t *bpb)
+{
+    return BPB_FAT_addr(bpb) + bpb->NumFATs * bpb->FATSz16 * bpb->BytsPerSec;
+}
+uint32_t BPB_Data_addr(struct BPB_t *bpb)
+{                               //size of root
+    return BPB_Root_addr(bpb) + bpb->RootEntCnt * 32;
+}
+uint32_t BPB_Data_Sector_count(struct BPB_t *bpb)
+{
+    return bpb->TotSec32 - BPB_Data_addr(bpb) / bpb->BytsPerSec;
+}
 
+struct dir_t {
+    char Name[11];
+    uint8_t Attr;
+    uint8_t NTRes;
+    uint8_t CrtTimeTenth;
+    uint16_t CrtTime;
 
-    char null;
+    uint16_t CrtDate;
+    uint16_t LstAccDate;
+    uint16_t FstClusHI;
+    uint16_t WrtTime;
+    uint16_t WrtDate;
+    uint16_t FstClusLO;
+    uint32_t FileSize;
+};
+enum dirattr_t {
+    DIR_ATTR_READONLY = 1,
+    DIR_ATTR_HIDDEN   = 1 << 1,
+    DIR_ATTR_SYSTEM   = 1 << 2,
+    DIR_ATTR_VOLUMEID = 1 << 3,
+    DIR_ATTR_DIRECTORY= 1 << 4,
+    DIR_ATTR_ARCHIVE  = 1 << 5,
+    DIR_ATTR_LFN      = 0xF
 };
 #pragma pack(pop)
+
+
+uint16_t get_next_cluster(FILE *f, struct BPB_t *bpb, uint16_t cluster)
+{
+    uint16_t result;
+    read_i16(f, BPB_FAT_addr(bpb) + cluster*2, &result);
+    debugf("next cluster: 0x%x -> 0x%x\n", cluster, result);
+    return result;
+}
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+void print_cluster(FILE *f, struct BPB_t *bpb, struct dir_t *dir)
+{
+    const uint32_t cluster_size = bpb->BytsPerSec * bpb->SecPerClus;
+
+    for(uint16_t c = dir->FstClusLO + (dir->FstClusHI << 8); c < 0xFFF8; c = get_next_cluster(f, bpb, c)) {
+        debugf("c = 0x%x, actual addr is 0x%x (which is incorrect).\n", c, BPB_Data_addr(bpb) + c*cluster_size);
+        debugf("subtracting 2 from cluster size (c = 0x%x) to get correct addr at 0x%x. ", c - 2, BPB_Data_addr(bpb) + (c-2)*cluster_size);
+#ifdef DEBUG_ON
+        redf("I HAVE NO IDEA WHY THIS WORKS!\n");
+#endif
+
+        char buf[cluster_size];
+        //TODO: i have *NO IDEA* why you have to subtract 2 from the cluster ID number
+        read_bytes(f, BPB_Data_addr(bpb) + (c - 2) * cluster_size, buf, sizeof(buf));
+
+        printf("%.*s", (int)(sizeof(buf)), buf);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -107,29 +181,34 @@ int main(int argc, char *argv[])
     if(bpb.BytsPerSec != 512 || bpb.NumFATs != 2)
         return 1;
 
-    uint32_t FirstFATSector = bpb.RsvdSecCnt;
-    uint32_t FirstRootSector = FirstFATSector + bpb.NumFATs*bpb.FATSz16;
-    uint32_t RootDirectorySectors = bpb.RootEntCnt*32/512;
-    uint32_t FirstDataSector = FirstRootSector + RootDirectorySectors;
-    uint32_t SectorCount = bpb.TotSec32 - FirstDataSector;
+    greenf( "FAT addr: 0x%x\n"
+            "Root addr: 0x%x\n"
+            "Data addr: 0x%x\n",
+            BPB_FAT_addr(&bpb),
+            BPB_Root_addr(&bpb),
+            BPB_Data_addr(&bpb)
+          );
 
     for(int i = 0; true; i++) {
-        uint32_t offset = FirstRootSector*bpb.BytsPerSec + i*32;
-        uint8_t first_byte;
-        read_i8(f, offset, &first_byte);
-        if(first_byte == 0)
+        struct dir_t dir;
+        uint32_t offset = BPB_Root_addr(&bpb) + i*32;
+        read_bytes(f, offset, &dir, sizeof(dir));
+
+        if(dir.Name[0] == 0)
             break;
 
-        uint8_t attrib;
-        read_i8(f, offset + 0x0B, &attrib);
-        if((attrib & 0xF) == 0xF) {
-            printf("long filename\n");
+        if(dir.Attr & DIR_ATTR_LFN) {
+            //printf("<long filename>\n");
             continue;
         }
 
-        char name[12];
-        read_bytes(f, offset, name, sizeof(name));
-        printf("%s\n", name);
+        purplef("\n%.*s <%s>\n", (int)(sizeof(dir.Name)/sizeof(char)), dir.Name, (dir.Attr & DIR_ATTR_DIRECTORY) ? "DIR" : "FILE");
+        if(!(dir.Attr & DIR_ATTR_DIRECTORY)) {
+            if(i == 7)
+                print_cluster(f, &bpb, &dir);
+            else
+                printf("<omitting print>\n");
+        }
     }
 
     fclose(f);
